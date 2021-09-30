fc2f2a90b93d96f6852b634942f8d63f97cb9c0d
Dimitar Damyanov <smgC8CB@smg.local>
2021-09-30 14:44:06 +0300

    Added solutions to loops.Task01 and modified branchedAlgoritms.Task02

--- src/branchedAlgorithms/Task02.java
+++ src/branchedAlgorithms/Task02.java
@@ -9,21 +9,20 @@ public class Task02 {
 		int sum = 0;
 		String isbnString = input.next();
 		input.close();
-		char[] isbn = new char[10];
+		char checkDigit;
 		
 		for (int i = 0; i < isbnString.length(); i++) {
 			sum += ((int) isbnString.charAt(i) - 48) * (i+1);
-			isbn[i] = isbnString.charAt(i);
 		}
 		
 		if (sum % 11 == 10) {
-			isbn[9] = 'X';
+			checkDigit = 'X';
 		} 
 		else {
-			isbn[9] = (char)(sum % 11 + 48);
+			checkDigit = (char)(sum % 11 + 48);
 		}
 		
-		System.out.println("ISBN: " + new String(isbn));
+		System.out.println("ISBN: " + isbnString + checkDigit);
 	}
 
 }
