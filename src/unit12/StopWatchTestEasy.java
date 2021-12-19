97b207e07a9e06a2ceff579c10cad5f5c32ecd22
Dimitar Damyanov <dim.damianov@gmail.com>
2021-12-19 15:25:59 +0200

    Added easy solution to StopWatchTest

---
+++ src/unit12/StopWatchTestEasy.java
@@ -0,0 +1,32 @@
+package unit12;
+
+public class StopWatchTestEasy {
+	public static void sort(int arr[])
+    {
+        int n = arr.length;
+ 
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+	
+	public static void main(String[] args) {
+		int[] array = new int[100000];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int) Math.random() * (int) Math.log10(Integer.MAX_VALUE);
+		}
+		StopWatch stopWatch = new StopWatch();
+		sort(array);
+		stopWatch.stop();
+		// System.out.printf("[%d...%d]\n", array[0], array[99999]);
+		System.out.println(stopWatch.getElapsedTime());
+	}
+}
