b121a2f1f96c207638a03dc357603c43d12efe8f
Dimitar Damyanov <dim.damianov@gmail.com>
2021-12-19 15:48:15 +0200

    Added easy optimized solution to 16. Task 02

--- src/unit16/Task02.java
+++ src/unit16/Task02.java
@@ -5,13 +5,13 @@ import java.math.BigInteger;
 public class Task02 {
 	public static void main(String[] args) {
 		BigInteger a = new BigInteger(String.valueOf(Long.MAX_VALUE));
-		BigInteger b = new BigInteger("1");
+		BigInteger big1 = new BigInteger("1");
 		BigInteger big0 = new BigInteger("0");
 		BigInteger big5 = new BigInteger("5");
 		BigInteger big6 = new BigInteger("6");
 		int n = 0;
 		while (n < 10) {
-			a = a.add(b);
+			a = a.add(big1);
 			if (a.divideAndRemainder(big5)[1].compareTo(big0) == 0) {
 				System.out.printf("%s mod 5 = 0\n", a);
 				n++;
