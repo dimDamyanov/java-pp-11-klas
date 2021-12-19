19fb06e7c8181b9036a34f62a07d7404cc77ddf7
Dimitar Damyanov <dim.damianov@gmail.com>
2021-12-19 15:37:59 +0200

    Added easy optimized solution to 16. Task 02

--- src/unit16/Task02.java
+++ src/unit16/Task02.java
@@ -6,14 +6,17 @@ public class Task02 {
 	public static void main(String[] args) {
 		BigInteger a = new BigInteger(String.valueOf(Long.MAX_VALUE));
 		BigInteger b = new BigInteger("1");
+		BigInteger big0 = new BigInteger("0");
+		BigInteger big5 = new BigInteger("5");
+		BigInteger big6 = new BigInteger("6");
 		int n = 0;
 		while (n < 10) {
 			a = a.add(b);
-			if (a.divideAndRemainder(new BigInteger("5"))[1].compareTo(new BigInteger("0")) == 0) {
+			if (a.divideAndRemainder(big5)[1].compareTo(big0) == 0) {
 				System.out.printf("%s mod 5 = 0\n", a);
 				n++;
 			}
-			else if (a.divideAndRemainder(new BigInteger("6"))[1].compareTo(new BigInteger("0")) == 0) {
+			else if (a.divideAndRemainder(big6)[1].compareTo(big0) == 0) {
 				System.out.printf("%s mod 6 = 0\n", a);
 				n++;
 			}
